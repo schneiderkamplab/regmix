de13c3257461a9bc9c397b9cfa0ae937394db2e7
Gianluca Barmina <gianlucabarmina@gmail.com>
2025-05-22 13:59:19 +0200

    Add dfm data preprocessing run script

---
+++ model_training/preprocess/run_preprocess_dfm.sh
@@ -0,0 +1,12 @@
+#export DATASET_SHORT_NAME="the_pile"
+export DATASET_SHORT_NAME="dfm"
+
+# python download_dataset.py --dataset_name sail/regmix-data-sample
+# WARNING: you can choose to download the full dataset (around 1TB) by running the following command
+# python download_data.py --dataset_name sail/regmix-data
+
+# We use gptneox tokenizer for the dataset to be consistent with the flagship method DoReMi
+python prepare_file_domain.py --source_path ./dfm_data --tokenizer_path tokenizer/gptneox --destination_path ./dfm_dataset_preproc --short_name $DATASET_SHORT_NAME --split train
+
+# 131136 = 2049 * 64, which means the chunk size is relatively smaller due to the size of the validation set, especially for low-resource domains
+python prepare_file_domain.py --source_path ./dfm_data --tokenizer_path tokenizer/gptneox --destination_path ./dfm_dataset_preproc --short_name $DATASET_SHORT_NAME --split valid --chunk_size 131136
